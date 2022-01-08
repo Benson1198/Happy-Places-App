1f1c85db1190766c256317f6bc2ae71bd8b7add2
Benson1198 <bensonpx1198@gmail.com>
2022-01-08 20:22:21 +0530

    Added DatePicker for DateEditText

--- app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,12 +1,18 @@
 package com.example.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener{
 
     private var binding: ActivityAddHappyPlaceBinding? = null
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +26,31 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         }
 
 
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR,year)
+            cal.set(Calendar.MONTH,month)
+            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateDateInView()
+        }
+        binding?.etDate?.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            binding?.etDate?.id ->{
+                DatePickerDialog(this@AddHappyPlaceActivity, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd-MMM-yyyy"
+        val sdf = SimpleDateFormat(myFormat,Locale.getDefault())
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
+
+    }
+
 }
