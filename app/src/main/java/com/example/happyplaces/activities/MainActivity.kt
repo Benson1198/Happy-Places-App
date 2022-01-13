c5e535c7e889986402a81e371dd0b703f12ae791
Benson1198 <bensonpx1198@gmail.com>
2022-01-13 12:16:57 +0530

    reading data from local db

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.happyplaces.activities.AddHappyPlaceActivity
+import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
+import com.example.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,8 +23,18 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+        getHappyPlaceListFromLocalDB()
+    }
+
+    private fun getHappyPlaceListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList:ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
 
-
+        if(getHappyPlaceList.size >0){
+            for(i in getHappyPlaceList){
+                Log.e("Title",i.title)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
+++ app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
@@ -1,9 +1,12 @@
 package com.example.happyplaces.database
 
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.happyplaces.models.HappyPlaceModel
 
@@ -65,5 +68,39 @@ class DatabaseHandler(context: Context) :
 
         db.close()
         return result
+    }
+
+    @SuppressLint("Range")
+    fun getHappyPlacesList():ArrayList<HappyPlaceModel>{
+        val happyPlaceList = ArrayList<HappyPlaceModel>()
+        val selectQuery = "SELECT * FROM $TABLE_HAPPY_PLACE"
+        val db = this.readableDatabase
+
+        try{
+            val cursor : Cursor = db.rawQuery(selectQuery,null)
+
+            if(cursor.moveToFirst()){
+                do{
+                    val place = HappyPlaceModel(
+                        cursor.getInt(cursor.getColumnIndex(KEY_ID)),
+                        cursor.getString(cursor.getColumnIndex(KEY_TITLE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_IMAGE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DATE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_LOCATION)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LATITUDE)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LONGITUDE)),
+                    )
+                    happyPlaceList.add(place)
+
+                }
+                    while(cursor.moveToNext())
+            }
+            cursor.close()
+        }catch(e:SQLiteException){
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+        return happyPlaceList
     }
 }
